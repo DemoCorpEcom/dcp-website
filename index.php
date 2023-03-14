6d1c246e8892e35fd2cd9f42e4486391180deede
Joshua Mathew <umjoshua@gmail.com>
2023-03-14 22:34:34 +0530

    cloudguard test

--- index.php
+++ index.php
@@ -12,7 +12,8 @@ include("server.php");
     <li><a href="/dcp/product.php?id=1">Item 1</a></li>
     <li><a href="/dcp//product.php?id=2">Item 2</a></li>
     <li><a href="/dcp/product.php?id=3">Item 3</a></li>
-    <a href="/dcp/product.php?redirectUrl=https://www.example.com"> Privacy Policy </a>
+    <p><a href="/dcp/product.php?redirectUrl=https://www.example.com"> Privacy Policy </a><p>
+    <p><a href="/dcp/product.php?redirect=https://www.example.com"> Terms & Conditions</a></p>
 </ul>
 
 </html>
