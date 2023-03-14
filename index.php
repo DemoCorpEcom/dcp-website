f72e39c9e7b23088cf55eaf40a3264697cb7159b
Joshua Mathew <umjoshua@gmail.com>
2023-03-14 22:08:25 +0530

    jenkins test

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ include("server.php");
     <li><a href="/dcp/product.php?id=1">Item 1</a></li>
     <li><a href="/dcp//product.php?id=2">Item 2</a></li>
     <li><a href="/dcp/product.php?id=3">Item 3</a></li>
-    <a href="/dcp/product.php?redirectUrl=https://www.example.com"> Privacy Policy </a>
+    <a href="/dcp/product.php?redirectUrl=https://www.google.com"> Privacy Policy </a>
 </ul>
 
 </html>
