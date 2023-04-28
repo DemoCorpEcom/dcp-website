e401f9a7bea2c6c3068b9e0b5cfd7619799912dc
Joshua Mathew <umjoshua@gmail.com>
2023-04-28 22:56:30 +0530

    ssrf vulnerable

--- image.php
+++ image.php
@@ -1,12 +1,7 @@
-
 <?php
-session_start();
-include("server.php");
-
 if (isset($_GET['image_url'])){
         $url = $_GET['image_url'];
         $file = fopen($url, 'rb');
-        header("Content-Type: image/jpeg");
         fpassthru($file);
     }
 ?>
