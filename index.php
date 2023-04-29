bb17ed8801c03d8936142afe4fe116916cb5cef9
Joshua Mathew <umjoshua@gmail.com>
2023-04-29 10:58:12 +0530

    opnrtat test

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ include("server.php");
     <li><a href="/dcp/product.php?id=1">Item 1</a></li>
     <li><a href="/dcp//product.php?id=2">Item 2</a></li>
     <li><a href="/dcp/product.php?id=3">Item 3</a></li>
-    <p><a href="/dcp/product.php?redirectUrl=https://www.umjoshua.xyz"> Privacy Policy </a><p>
+    <p><a href="/dcp/product.php?redirectUrl=https://www.google.com"> Privacy Policy </a><p>
 </ul>
 
 <!-- <img src="/dcp/image.php?image_url=https://www.w3schools.com/html/img_girl.jpg"></img> -->
